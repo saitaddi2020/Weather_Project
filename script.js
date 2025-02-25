74442a0982b893c4c3b33e5655d912e8208e7b13
saitaddi2020 <saitaddi2020@gmail.com>
2025-02-25 19:05:04 +0530

    This is a Weather forecast project

---
+++ script.js
@@ -0,0 +1,57 @@
+const apiKey = "a6c02a6319e674e972890fbcc87910b3"
+
+const weatherDataEle = document.querySelector(".weather-data")
+const cityNameEle = document.querySelector("#city-name")
+const formEle = document.querySelector("form")
+const imgIcon = document.querySelector(".icon")
+
+formEle.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const cityValue = cityNameEle.value
+
+    getWeatherData(cityValue)
+})
+
+async function getWeatherData(cityValue){
+    try{
+        const response =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`)
+        if(!response.ok){
+            throw new Error("Network response is not ok!")
+        }
+
+        const data = await response.json()
+        console.log(data);
+
+        const temprature = Math.floor(data.main.temp)
+        const description = data.weather[0].description
+        const icon = data.weather[0].icon
+        const longLat = `Latitude: ${data.coord.lat}, Longitude: ${data.coord.lon}`;
+
+        const details = [
+            `Feels Like: ${Math.floor(data.main.feels_like)}°C`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind Speed: ${data.wind.speed} m/s`,
+            longLat
+        ]
+
+        weatherDataEle.querySelector(".temp").textContent = `${temprature}°C`
+        weatherDataEle.querySelector(".desc").textContent = `${description}`
+
+        imgIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}.png" alt="">`
+
+        weatherDataEle.querySelector(".details").innerHTML = details.map((detail)=>{
+           return `<div>${detail}</div>`
+        }).join("")
+
+        weatherDataEle.querySelector(".details").style.display = "flex";
+       
+
+    }catch(err){
+        weatherDataEle.querySelector(".temp").textContent = ""
+        imgIcon.innerHTML = ""
+        weatherDataEle.querySelector(".desc").textContent = "An Error Occurred!"
+        weatherDataEle.querySelector(".details").style.display = "none";
+
+    }
+
+}
